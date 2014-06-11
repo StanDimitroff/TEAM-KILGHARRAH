a6fc14eecc1f07f2472a4d4e742c1a8ca388bd7e
Tsvetina Georgieva <tsv.georgieva@abv.bg>
2014-06-11 11:23:22 +0300

    added mind map of project

---
+++ SoftUni Survival Guide.mm
@@ -0,0 +1,21 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1402474506452" ID="ID_1268366151" MODIFIED="1402474622864" STYLE="bubble" TEXT="SoftUni Survival Guide">
+<node CREATED="1402474534191" ID="ID_1107237321" MODIFIED="1402474622864" POSITION="right" TEXT="Homepage"/>
+<node CREATED="1402474552875" ID="ID_556101872" MODIFIED="1402474644870" POSITION="right" TEXT="About"/>
+<node CREATED="1402474657433" ID="ID_719720581" MODIFIED="1402474750960" POSITION="right" TEXT="&#x41b;&#x435;&#x43a;&#x442;&#x43e;&#x440;&#x438;">
+<node CREATED="1402474746481" ID="ID_1013830178" MODIFIED="1402474771502" TEXT="&#x421;&#x43d;&#x438;&#x43c;&#x43a;&#x430;">
+<node CREATED="1402474801912" ID="ID_1781180567" MODIFIED="1402474812662" TEXT="photoshoped"/>
+</node>
+<node CREATED="1402474759457" ID="ID_1480652144" MODIFIED="1402474777855" TEXT="&#x418;&#x43c;&#x435;"/>
+<node CREATED="1402474754369" ID="ID_1247757825" MODIFIED="1402474788510" TEXT="&#x41d;&#x430;&#x432;&#x438;&#x446;&#x438;"/>
+<node CREATED="1402474791721" ID="ID_1483535985" MODIFIED="1402474796999" TEXT="&#x411;&#x43e;&#x43b;&#x43d;&#x430; &#x442;&#x435;&#x43c;&#x430;"/>
+</node>
+<node CREATED="1402474655003" ID="ID_1935699107" MODIFIED="1402474688031" POSITION="right" TEXT="&#x41b;&#x435;&#x43a;&#x446;&#x438;&#x438; &#x438; &#x434;&#x43e;&#x43c;&#x430;&#x448;&#x43d;&#x438;"/>
+<node CREATED="1402474652978" ID="ID_1250296916" MODIFIED="1402474702488" POSITION="right" TEXT="&#x421;&#x44a;&#x432;&#x435;&#x442;&#x438; &#x437;&#x430; &#x43e;&#x446;&#x435;&#x43b;&#x44f;&#x432;&#x430;&#x43d;&#x435;">
+<node CREATED="1402474848009" ID="ID_1658183238" MODIFIED="1402474851004" TEXT="&#x41d;&#x435; &#x445;&#x43e;&#x434;&#x438; &#x43d;&#x430; &#x43b;&#x435;&#x43a;&#x446;&#x438;&#x44f;, &#x430;&#x43a;&#x43e; &#x43d;&#x435; &#x441;&#x438; &#x43f;&#x438;&#x43b;. &#x410;&#x43a;&#x43e; &#x441;&#x43b;&#x443;&#x447;&#x430;&#x44f;&#x442; &#x435; &#x442;&#x430;&#x43a;&#x44a;&#x432;, &#x437;&#x430;&#x434;&#x44a;&#x43b;&#x436;&#x438;&#x442;&#x435;&#x43b;&#x43d;&#x43e; &#x442;&#x440;&#x44f;&#x431;&#x432;&#x430; &#x434;&#x430; &#x441;&#x438; &#x432;&#x437;&#x435;&#x43c;&#x435;&#x448; &#x43f;&#x43e;&#x43d;&#x435; 5 &#x431;&#x438;&#x440;&#x438; &#x43e;&#x442; &#x43a;&#x430;&#x444;&#x435;&#x442;&#x43e;. "/>
+<node CREATED="1402474872690" ID="ID_1234222343" MODIFIED="1402474874440" TEXT="private browsing"/>
+</node>
+<node CREATED="1402474650715" ID="ID_1570699150" MODIFIED="1402474725210" POSITION="right" TEXT="&#x418;&#x43d;&#x442;&#x435;&#x440;&#x432;&#x44e;&#x442;&#x430; &#x441;&#x44a;&#x441; &#x441;&#x442;&#x443;&#x434;&#x435;&#x43d;&#x442;&#x438;"/>
+</node>
+</map>
